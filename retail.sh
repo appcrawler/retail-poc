fdb2bdaebe3087028956cca16168dcd9747f32d8
appcrawler <stevedhoward@gmail.com>
2020-03-23 10:59:06 -0400

    Update retail.sh
    Remove references to products, which aren't used (but could be) in this demo

--- retail.sh
+++ retail.sh
@@ -1,5 +1,6 @@
 #---------------------------------------------------------------------------------
 # Stop anything currently running on this host and delete for fresh start
+# change PATH to confluent executable below to wherever you have the platform installed
 export PATH=/root/confluent-5.4.0/bin:$PATH
 confluent local stop
 rm -rf /tmp/confluent.*
@@ -13,7 +14,6 @@ confluent local start
 #---------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------
-kafka-topics --bootstrap-server localhost:9092 --delete --topic products
 kafka-topics --bootstrap-server localhost:9092 --delete --topic inventory
 kafka-topics --bootstrap-server localhost:9092 --delete --topic sales
 kafka-topics --bootstrap-server localhost:9092 --delete --topic receipts
@@ -22,6 +22,5 @@ kafka-topics --bootstrap-server localhost:9092 --delete --topic SOH
 kafka-topics --bootstrap-server localhost:9092 --create --topic sales --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server localhost:9092 --create --topic receipts --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server localhost:9092 --create --topic inventory --partitions 1 --replication-factor 1
-kafka-topics --bootstrap-server localhost:9092 --create --topic products --partitions 1 --replication-factor 1
 
 cat retail.ksql | ksql
